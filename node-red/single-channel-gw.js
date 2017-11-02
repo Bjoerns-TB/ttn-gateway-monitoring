7334d1eace2670f2e977c8ef407b17d1da426ba8
Bjoerns-TB <30447739+Bjoerns-TB@users.noreply.github.com>
2017-11-02 16:10:00 +0100

    Update single-channel-gw.js

--- node-red/single-channel-gw.js
+++ node-red/single-channel-gw.js
@@ -5,7 +5,7 @@
         "z": "7204abf1.ff7124",
         "name": "",
         "server": "client",
-        "host": "yout GW IP address",
+        "host": "your GW IP",
         "port": "23",
         "datamode": "stream",
         "datatype": "utf8",
@@ -13,6 +13,30 @@
         "topic": "",
         "base64": false,
         "x": 106.5,
+        "y": 23,
+        "wires": [
+            [
+                "d28c9cef.dbfba"
+            ]
+        ]
+    },
+    {
+        "id": "d28c9cef.dbfba",
+        "type": "switch",
+        "z": "7204abf1.ff7124",
+        "name": "",
+        "property": "payload",
+        "propertyType": "msg",
+        "rules": [
+            {
+                "t": "cont",
+                "v": "message",
+                "vt": "str"
+            }
+        ],
+        "checkall": "true",
+        "outputs": 1,
+        "x": 291,
         "y": 23,
         "wires": [
             [
@@ -28,8 +52,8 @@
         "func": "var msg1 = {};\nvar msg2 = {};\nmsg.payload = msg.payload.split(\"message\");\nmsg1.payload = msg.payload[1];\nmsg1.payload = msg1.payload.split(\"length\");\nmsg2.payload = msg1.payload[0];\nreturn msg2;",
         "outputs": 1,
         "noerr": 0,
-        "x": 275.5,
-        "y": 23,
+        "x": 441.5,
+        "y": 25,
         "wires": [
             [
                 "a2155487.ed9c28"
@@ -41,8 +65,8 @@
         "type": "json",
         "z": "7204abf1.ff7124",
         "name": "",
-        "x": 392.5,
-        "y": 23,
+        "x": 558.5,
+        "y": 25,
         "wires": [
             [
                 "a8f80737.6a73d8"
@@ -57,8 +81,8 @@
         "func": "//rssi\nvar msg1 = { payload: msg.payload.length };\nmsg1.payload = msg.payload.rxpk[0].rssi;\n\n//snr\nvar msg2 = { payload: msg.payload.length };\nmsg2.payload = msg.payload.rxpk[0].lsnr;\n\n//freq\nvar msg3 = { payload: msg.payload.length };\nmsg3.payload = msg.payload.rxpk[0].freq;\n\n//size\nvar msg4 = { payload: msg.payload.length };\nmsg4.payload = msg.payload.rxpk[0].size;\n\n//SF\nvar msg5 = { payload: msg.payload.length };\nmsg5.payload = msg.payload.rxpk[0].datr;\nmsg5.payload = msg5.payload.split(\"BW\");\nmsg5.payload = msg5.payload[0];\nmsg5.payload = msg5.payload.split(\"SF\");\nmsg5.payload = msg5.payload[1];\n\n//size\nvar msg7 = { payload: msg.payload.length };\nmsg7.payload = msg.payload.rxpk[0].chan;\n\nvar msg6 = {};\nmsg6.payload = [{\"rssi\": msg1.payload, \"snr\": msg2.payload, \"freq\": msg3.payload, \"size\": msg4.payload, \"sf\": msg5.payload, \"chan\": msg7.payload}];\n\nreturn msg6;",
         "outputs": "1",
         "noerr": 0,
-        "x": 513.5,
-        "y": 23,
+        "x": 679.5,
+        "y": 25,
         "wires": [
             [
                 "e04dadbe.cdff7"
@@ -74,8 +98,8 @@
         "measurement": "gw0",
         "precision": "",
         "retentionPolicy": "",
-        "x": 705.5,
-        "y": 24,
+        "x": 871.5,
+        "y": 26,
         "wires": []
     },
     {
